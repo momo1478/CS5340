c7d43f9810ad8224c239717362ad3b45b7672445
monishg <monishg@lab1-17.eng.utah.edu>
2018-09-13 01:09:00 -0600

    making progress 2.0

--- A1/morphology.py
+++ A1/morphology.py
@@ -1,18 +1,27 @@
-import sys;
+import sys
+from collections import defaultdict
 
-dictionary = {} # word -> POS
-roots = {}      # word -> root
-rules = []      # (FIX, actual fix, replace letters, POS origin, POS derived)
+dictionary = defaultdict(list) # word -> [(POS , ?Root)]
+rules = []        # (FIX, actual fix, replace letters, POS origin, POS derived)
 tests = []
 
-def analyze(word,firstCall,POS):
-    #result = [word,POS,root,source]
-
+def analyze(word, originPOS, derivedPOS):
     if(word in dictionary):
-        source = ("dictionary" if firstCall else "morphology")
-        return [word,dictionary[word],word,source]
-    elif(word in roots):
-        return analyze(roots[word], True, dictionary[roots[word]])
+        root = word
+        source = "morphology"
+        if(len(dictionary[word][0]) == 2): # Has Root
+            root = dictionary[word][0][1]
+            POS  = dictionary[word][0][0]
+        else:
+            POS = dictionary[word][0]
+        
+        # Match POS in dictionary
+        valid = False
+        for entry in dictionary[word]:
+            if(originPOS == entry[0]):
+                valid = True
+
+        return [word,derivedPOS,root,source] if valid else None
     else:
         for rule in rules:
             if(rule[0] == "PREFIX"):
@@ -20,27 +29,87 @@ def analyze(word,firstCall,POS):
                     newWord = word[len(rule[1]):]
                     if(rule[2] != "-"):
                         newWord = rule[2] + newWord
-                    return analyze(newWord, False, POS = rule[4])
+                    return analyze(newWord,rule[3],rule[4])
             else:
                 if(word.endswith(rule[1])):
                     newWord = word[:-len(rule[1])]
                     if(rule[2] != "-"):
                         newWord = newWord + rule[2]
-                    return analyze(newWord, False, POS = rule[4])
-        
-        # No rules apply
-        source = "default"
-        dictionary[word] = "noun"
-        return [word, "noun", word, source]
+                    return analyze(newWord,rule[3],rule[4])
+    
+def analyzeWord(word):
+    if(word in dictionary):
+        root = word
+        source = "dictionary"
+        if(len(dictionary[word][0]) == 2): # Has Root
+            root = dictionary[word][0][1]
+            POS  = dictionary[word][0][0]
+        else:
+            POS = dictionary[word][0]
 
+        return [word,POS,root,source]
+    else:
+        results = []
+        for rule in rules:
+            if(rule[0] == "PREFIX"):
+                if(word.startswith(rule[1])):
+                    newWord = word[len(rule[1]):]
+                    if(rule[2] != "-"):
+                        newWord = rule[2] + newWord
+                    results.append(analyze(newWord,rule[3],rule[4]))
+            else:
+                if(word.endswith(rule[1])):
+                    newWord = word[:-len(rule[1])]
+                    if(rule[2] != "-"):
+                        newWord = newWord + rule[2]
+                    results.append(analyze(newWord,rule[3],rule[4]))
+        
+        if(all(result is None for result in results)):
+            return [word,"noun",word,"default"]
+        else:
+            return results
+ 
+    # if(word in dictionary or word in roots):
+    #     if(word in roots):
+    #         POS = rootPOS[word]
+    #         word = roots[word]
+    #         firstCall = False
+    #     else:
+    #         POS = dictionary[word]
+    #     source = ("dictionary" if firstCall else "morphology")
+    #     return [word,dictionary[word],word,source]
+    # else:
+    #     results = []
+    #     for rule in rules:
+    #         if(rule[0] == "PREFIX"):
+    #             if(word.startswith(rule[1])):
+    #                 newWord = word[len(rule[1]):]
+    #                 if(rule[2] != "-"):
+    #                     newWord = rule[2] + newWord
+    #                 results.append(analyze(newWord, False, rule[4]))
+    #         else:
+    #             if(word.endswith(rule[1])):
+    #                 newWord = word[:-len(rule[1])]
+    #                 if(rule[2] != "-"):
+    #                     newWord = newWord + rule[2]
+    #                 results.append(analyze(newWord, False, rule[4]))
+    #     print(results)
+    #     if(all(result is None for result in results) and firstCall):
+    #         # No rules apply
+    #         source = "default"
+    #         return [word, "noun", word, source]
+    #     if(any(result is not None for result in results)):
+    #         newRes = filter(None, results)
+    #         return newRes
+    #     return None
 
 with open(sys.argv[1],'r') as f:
     for line in f:
         sp = line.split()
         if  (len(sp) == 2):
-            dictionary[sp[0]] = sp[1]
+            dictionary[sp[0]].append((sp[1]))
         elif(len(sp) == 4):
-            roots[sp[0]] = sp[3]
+            dictionary[sp[0]].append((sp[1],sp[3]))
 
 with open(sys.argv[2],'r') as f:
     for line in f:
@@ -53,6 +122,15 @@ with open(sys.argv[3],'r') as f:
         tests.append(line.strip())
 
 for word in tests:
-    result = analyze(word, firstCall = True, POS = "")
-    form = "{0: <15} {1: <15} ROOT={2: <15} SOURCE={3: <15}"
-    print(form.format(word,result[1],result[2],result[3]))
+    results = analyzeWord(word)
+    print(results)
+    # if(isinstance(results[0],list) == False):
+    #     form = "{0: <15} {1: <15} ROOT={2: <15} SOURCE={3: <15}"
+    #     print(form.format(word,results[1],results[2],results[3]))
+    # else:
+    #     for result in results:
+    #         form = "{0: <15} {1: <15} ROOT={2: <15} SOURCE={3: <15}"
+    #         print(form.format(word,results[1],results[2],results[3]))
+
+    print("")
+    
